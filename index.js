b6d0cc0a5f00c7e8861675d1684cead3b2b89178
msyk <nii@msyk.net>
2022-10-01 18:43:33 +0900

    Setup

--- index.js
+++ index.js
@@ -8,16 +8,19 @@
  * https://github.com/INTER-Mediator/INTER-Mediator/blob/master/dist-docs/License.txt
  */
 IMParts_Catalog.mermaid = {
+  // https://github.com/mermaid-js/mermaid/blob/develop/docs/usage.md
+  options: {
+    startOnLoad: false
+  },
+
   instanciate: function (parentNode) {
     'use strict'
     const classOfParent = parentNode.getAttribute('class')
-    if (classOfParent.indexOf('_im_widget_mermaid') < 0) {
-      const sp = classOfParent.length == 0 ? '' : ' '
+    if (!classOfParent || classOfParent.length == 0 || classOfParent.indexOf('_im_widget_mermaid') < 0) {
+      const sp = (classOfParent && classOfParent.length > 0) ? ' ' : ''
       parentNode.setAttribute('class', `${classOfParent}${sp}_im_widget_mermaid`)
       const node = document.createElement('DIV')
-      node.setAttribute('data-im-control', 'enclosure')
-      node.setAttribute('class', '_im_widget_mermaid_canvas')
-      const newId = parentNode.getAttribute('id') + '-popupsel'
+      const newId = parentNode.getAttribute('id') + '-mermaid'
       node.setAttribute('id', newId)
       parentNode.appendChild(node)
       IMParts_Catalog.mermaid.ids.push(newId)
@@ -31,23 +34,28 @@ IMParts_Catalog.mermaid = {
 
       parentNode._im_setValue = (function () {
         const theId = newId
+        const target = node
         return function (str) {
-          mermaidAPI.initialize({
-            startOnLoad: true,
-          });
-          $(function () {
-            mermaidAPI.render(theId, str, function (svgGraph) {
-              console.log(svgGraph);
-            });
-          });
+          IMParts_Catalog.mermaid.values[theId] = str
         }
       })()
     }
   },
 
   ids: [],
+  values: [],
 
   finish: function () {
     'use strict'
+    mermaid.initialize(IMParts_Catalog.mermaid.options);
+    for (const id in IMParts_Catalog.mermaid.values) {
+      if (IMParts_Catalog.mermaid.values[id].length > 0) {
+        const node = document.getElementById(id)
+        const parent = node.parentNode
+        mermaid.mermaidAPI.render(id, IMParts_Catalog.mermaid.values[id], function (svgCode, bindFunctions) {
+          parent.innerHTML = svgCode
+        });
+      }
+    }
   }
 }
